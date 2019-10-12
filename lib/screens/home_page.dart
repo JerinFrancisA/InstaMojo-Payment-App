70ac320881a3613bcc181910dc751121019cc8fd
JerinFrancisA <jerinfrancis77@gmail.com>
2019-10-12 15:46:01 +0530

    login success

---
+++ lib/screens/home_page.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class HomePage extends StatefulWidget {
+  HomePage({Key key}) : super(key: key);
+  static const routeName ='HomePage';
+  @override
+  _HomePageState createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
--- lib/screens/login_screen.dart
+++ lib/screens/login_screen.dart
@@ -1,6 +1,8 @@
 import 'package:flutter/material.dart';
 import 'package:folk_payments/custom_widgets/input_box.dart';
+import 'package:folk_payments/screens/home_page.dart';
 import 'package:folk_payments/custom_widgets/button.dart';
+import 'package:firebase_auth/firebase_auth.dart';
 
 class LoginScreen extends StatefulWidget {
   LoginScreen({Key key}) : super(key: key);
@@ -13,19 +15,35 @@ class LoginScreen extends StatefulWidget {
 class _LoginScreenState extends State<LoginScreen> {
   var un = InputBox(text: 'Folk ID');
   var pw = InputBox(text: 'Folk Password');
+  final auth = FirebaseAuth.instance;
+  AuthResult user;
 
   @override
   Widget build(BuildContext context) {
     return SafeArea(
       child: Scaffold(
         appBar: AppBar(),
-        body: Column(
-          children: <Widget>[
-            un,
-            pw,
-            Button(
-              text: 'LOGIN',
-              onPressed: () {},
+        body: ListView(
+          children: [
+            Column(
+              children: <Widget>[
+                un,
+                pw,
+                Button(
+                  text: 'LOGIN',
+                  onPressed: () async {
+                    try {
+                      user = await auth.signInWithEmailAndPassword(email: un.input, password: pw.input);
+                      if(user!=null) {
+                        Navigator.of(context).pop();
+                        Navigator.pushNamed(context, HomePage.routeName);
+                      }
+                    } catch(e) {
+                      print(e);
+                    }
+                  },
+                ),
+              ],
             ),
           ],
         ),
